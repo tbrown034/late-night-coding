f174ddaefe5e5d9c5d029303fbaaf990f9f44afa
tbrown034 <tbrown034@gmail.com>
2023-12-06 03:30:29 -0500

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// selectors
+const startGame = document.getElementById("start-game");
+const startMessage = document.getElementById("start-message");
+const gameBoard = document.getElementById("game-board");
+const resultsMessage = document.getElementById("results-message");
+const restartButton = document.getElementById("restart-button");
+
+// event listeners
+
+startGame.addEventListener("click", handleStartGame);
